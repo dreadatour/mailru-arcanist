df5a388d1bc6b4fc131491d9eae14d05aca5e01c
Pavel Zinovkin <pzinovkin@gmail.com>
2014-04-18 19:10:07 +0400

    Silence will fall

--- getarcanist.sh
+++ getarcanist.sh
@@ -3,6 +3,6 @@ set -e
 
 INSTALL_SCRIPT="/tmp/installarcanist.sh"
 
-curl -L https://raw.github.com/pzinovkin/mailru-arcanist/master/installarcanist.sh -o $INSTALL_SCRIPT
+curl -sL https://raw.github.com/pzinovkin/mailru-arcanist/master/installarcanist.sh -o $INSTALL_SCRIPT
 /bin/bash $INSTALL_SCRIPT
 rm $INSTALL_SCRIPT

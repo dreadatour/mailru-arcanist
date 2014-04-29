18206871554b978d7d3a541bc20dea2a8f7f2cb6
Pavel Zinovkin <pzinovkin@gmail.com>
2014-04-29 16:58:28 +0400

    Well

--- getarcanist.sh
+++ getarcanist.sh
@@ -3,6 +3,6 @@ set -e
 
 INSTALL_SCRIPT="/tmp/installarcanist.sh"
 
-curl -sL https://raw.github.com/pzinovkin/mailru-arcanist/master/installarcanist.sh -o $INSTALL_SCRIPT
+curl -sL https://raw.githubusercontent.com/pzinovkin/mailru-arcanist/master/installarcanist.sh -o $INSTALL_SCRIPT
 /bin/bash $INSTALL_SCRIPT
 rm $INSTALL_SCRIPT
